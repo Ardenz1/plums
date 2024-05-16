bc0810eddbb5cd0c68791f134d630d324764b70c
emmaohn <joh20123@byui.edu>
2024-05-16 14:49:44 -0600

    lot of path updates, topiccard name updated

--- src/components/TopicCard.jsx
+++ src/components/TopicCard.jsx
@@ -1,5 +1,5 @@
 
-export default function BackButton({ topicName }) {
+export default function TopicCard({ topicName }) {
   return (
     <div className="flex justify-between items-center bg-leaf-200 p-5 rounded-2xl mb-2">
       <h2>{topicName}</h2>
