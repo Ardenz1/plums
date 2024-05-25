6b2859a7e5b30899865293003462e7eb03892f43
emmaohn <joh20123@byui.edu>
2024-05-25 12:18:40 -0600

    Database files added

---
+++ src/database/sampledata.js
@@ -0,0 +1,9 @@
+// sample data goes here!
+
+// example of creating data with prisma!!
+// const user = await prisma.user.create({
+//   data: {
+//     email: 'elsa@prisma.io',
+//     name: 'Elsa Prisma',
+//   },
+// })
