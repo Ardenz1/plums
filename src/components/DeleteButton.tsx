f01d6d6313af1701114092353dc9f5baa473a3a3
arden <ardenbyui.edu>
2024-05-29 09:36:19 -0600

    singleNote and edit/delete btns

---
+++ src/components/DeleteButton.tsx
@@ -0,0 +1,7 @@
+export default function DeleteButton() {
+    return (
+      <div>
+     <button className="bg-leaf-200 rounded-2xl p-3 px-6 mb-1 text-leaf-300 font-bold">Delete</button>
+      </div>
+    )
+  }
