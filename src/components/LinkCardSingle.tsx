f1b98925c12d364acbbc08c1d1b99a615ec479b8
arden <ardenbyui.edu>
2024-06-01 16:49:22 -0600

    small link change

--- src/components/LinkCardSingle.tsx
+++ src/components/LinkCardSingle.tsx
@@ -10,20 +10,12 @@ export interface Props {
 const LinkCardSingle = (props: Props) => {
   // database call for number of each items should go here
   return (
-    <Fragment>
-      <div className="items-center bg-plum-100 p-5 rounded-2xl mb-2">
-        <h2 className="text-sm text-plum-300 font-bold">{props.title}</h2>
-        <h3 className="text-xs text-plum-300 font-thin ">
-          {props.link_created_at}
-        </h3>
-        <a className="text-sm text-plum-300 pt-2 line-clamp-4 underline">
-          {props.link}
-        </a>
-        <p className="text-sm text-plum-300 pt-2 line-clamp-4">
-          {props.description}
-        </p>
-      </div>
-    </Fragment>
+    <div className="items-center bg-plum-100 p-5 rounded-2xl mb-2">
+    <h2 className="text-sm text-plum-300 font-bold">{props.title}</h2>
+    <h3 className="text-xs text-plum-300 font-thin ">{props.link_created_at}</h3>
+    <a className="text-sm text-plum-300 pt-2 line-clamp-4 underline">{props.link}</a>
+    <p className="text-sm text-plum-300 pt-2 line-clamp-4">{props.description}</p>
+</div>
   );
 };
 
