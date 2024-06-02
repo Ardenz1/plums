44e173feda06bb4968f72e235beb9fac6d800c2e
emmaohn <joh20123@byui.edu>
2024-06-01 18:46:45 -0600

    updated delete and edit button

--- src/components/DeleteEditBtns.tsx
+++ src/components/DeleteEditBtns.tsx
@@ -1,12 +1,8 @@
 export default function DeleteEditBtns() {
   return (
-    <>
-      <div className="absolute bottom-0">
-        <div className="mt-auto flex justify-center items-center py-2 space-x-2">
-          <button className="bg-leaf-200 rounded-2xl p-3 px-6 mb-1 text-leaf-300 font-bold">Delete</button>
-          <button className="border-4 border-leaf-200 rounded-2xl p-2 px-8 mb-1 text-leaf-300 font-bold">Edit</button>
-        </div>
-      </div>
-    </>
+    <div className=" bg-white sticky bottom-0 mt-auto flex justify-center items-center space-x-2">
+      <button className="bg-leaf-200 rounded-2xl my-3 p-3 px-6 text-leaf-300 font-bold">Delete</button>
+      <button className="border-4 border-leaf-200 rounded-2xl my-3 p-2 px-8 text-leaf-300 font-bold">Edit</button>
+    </div>
   )
 }
