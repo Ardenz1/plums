5033003334e83cc23ab1efbfd79b095f413ff36c
emmaohn <joh20123@byui.edu>
2024-06-06 14:45:33 -0600

    Post and get functions for get all topics and topic details by topic id

---
+++ src/app/page.jsx
@@ -0,0 +1,24 @@
+import TopicCard from "../components/TopicCard";
+import { getAllTopics } from "@/database/database";
+
+export default async function Home() {
+  // database call!!
+  let topicList = await getAllTopics();
+  
+  return (
+    <main>
+      <h1>Topics</h1>
+      {/* dynamically generating cards from database data!! */}
+      {
+        topicList.map(topic => {
+          return (
+            <TopicCard 
+              key={topic.topic_id}
+              topicName={topic.topic_name}
+            />
+          )
+        })
+      }
+    </main>
+  );
+}
