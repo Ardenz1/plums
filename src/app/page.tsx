c44e1bbcd318242b8935b8ffa87cff1cbc1561db
arden <ardenbyui.edu>
2024-06-06 15:16:46 -0600

    subtopic card and subtopic btn styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import TopicCard from "../components/TopicCard";
+import SubTopicCard from "@/components/SubTopicCard";
 
 export default function Home() {
   return (
@@ -7,8 +8,9 @@ export default function Home() {
       <h1>Topics</h1>
       {/* this is an example to show what the component looks like */}
       {/* from database info, we can generate topic cards dynamically */}
-      <TopicCard topicName="React" />
-      <TopicCard topicName="Type Script" />
+      <TopicCard topicName="React" parentId={1} />
+      <SubTopicCard topicName="syntax" />
+      <TopicCard topicName="Type Script" parentId={null}/>
     </main>
   );
 }
--- src/components/TopicCard.tsx
+++ src/components/TopicCard.tsx
@@ -1,15 +1,29 @@
 
 export interface Props {
-  topicName: string;
+  topicName: string,
+  parentId: number | null;
 }
 
 const TopicCard = (props: Props) => {
+  if(props.parentId == null) {
   return (
     <div className="flex justify-between items-center bg-leaf-200 p-5 rounded-2xl mb-2">
       <h2>{props.topicName}</h2>
       <i className="fa-solid fa-caret-right"></i>
     </div>
   )
+}
+else{
+  return (
+ <div className="flex justify-between items-center bg-leaf-200 p-5 rounded-2xl mb-2">
+    <h2>{props.topicName}</h2>
+    <button> 
+      <span className="flex justify-center items-center w-6 h-6 bg-leaf-100 rounded-full">
+      <i className="fa-solid fa-caret-down"></i>
+      </span>
+    </button>
+</div>
+  )}
 }
 
 export default TopicCard;
