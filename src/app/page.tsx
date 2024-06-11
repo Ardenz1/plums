961e4bccb22597d0f11ac1fb8ee682e14374be5c
emmaohn <joh20123@byui.edu>
2024-06-11 15:38:11 -0600

    fixed topic page tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,33 @@
-import Image from "next/image";
 import TopicCard from "../components/TopicCard";
 import SubTopicCard from "@/components/SubTopicCard";
 
-export default function Home() {
+import { getAllTopics } from "@/database/database";
+import { Topic } from "@prisma/client";
+
+export default async function Home() {
+  // database call!!
+  let topicList: Topic[] = await getAllTopics();
+  
   return (
-    <main className="">
+    <main>
       <h1>Topics</h1>
-      {/* this is an example to show what the component looks like */}
-      {/* from database info, we can generate topic cards dynamically */}
-      <TopicCard topicName="React" parentId={1} />
-      <SubTopicCard topicName="syntax" />
-      <TopicCard topicName="Type Script" parentId={null}/>
+      {/* dynamically generating cards from database data!! */}
+      {
+        topicList.map(topic => {
+          return (
+            <TopicCard 
+              key={topic.topic_id}
+              topicId={topic.topic_id}
+              topicName={topic.topic_name}
+              // Jojo's topic column does not know????
+              parentId={1}
+            />
+          )
+            // <TopicCard topicName="React" parentId={1} />
+            // <SubTopicCard topicName="syntax" />
+            // <TopicCard topicName="Type Script" parentId={null}/>
+        })
+      }
     </main>
   );
 }
