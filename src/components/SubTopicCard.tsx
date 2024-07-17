8ddff341bf61723c3667645bd846153b38abbfc5
emmaohn <joh20123@byui.edu>
2024-07-16 23:18:41 -0600

    fixed hydration errors

--- src/components/SubTopicCard.tsx
+++ src/components/SubTopicCard.tsx
@@ -1,63 +1,62 @@
-'use client';
+"use client";
 
 import { useEffect } from "react";
 
 export interface Props {
-    topicName: string;
-    parentId: number | null;
-    topicId: number;
-    hasSubTopics: boolean;
-    subTopics?: string[];
-  }
-  
- 
-  const SubTopicCard = (props: Props) => {
-    useEffect(() => {
-      hideSubTopics(); // Hide subtopics when component mounts
-    }, []); // Empty dependency array ensures this runs only once on mount
-  
-    const hideSubTopics = () => {
-      let subTopics = document.querySelectorAll(`.subtopic-${props.topicId}`);
-      subTopics.forEach(subTopic => {
-        subTopic.classList.add('hidden');
-      });
-    };
-  
-    const toggleSubTopics = () => {
-      let subTopics = document.querySelectorAll(`.subtopic-${props.topicId}`);
-      subTopics.forEach(subTopic => {
-        subTopic.classList.toggle('hidden');
-      });
-    };
-  
-    return (
-      <div>
-        <div id={`subtopic-${props.topicId}`} className="flex justify-between items-center bg-plum-100 p-4 rounded-2xl mb-2 subtopic">
-          <a href={`/topicDetails/${props.topicId}`} className="flex-grow">
+  topicName: string;
+  parentId: number | null;
+  topicId: number;
+  hasSubTopics: boolean;
+  subTopics?: string[];
+}
+
+const SubTopicCard = (props: Props) => {
+  useEffect(() => {
+    hideSubTopics(); // Hide subtopics when component mounts
+  }, []); // Empty dependency array ensures this runs only once on mount
+
+  const hideSubTopics = () => {
+    let subTopics = document.querySelectorAll(`.subtopic-${props.topicId}`);
+    subTopics.forEach((subTopic) => {
+      subTopic.classList.add("hidden");
+    });
+  };
+
+  const toggleSubTopics = () => {
+    let subTopics = document.querySelectorAll(`.subtopic-${props.topicId}`);
+    subTopics.forEach((subTopic) => {
+      subTopic.classList.toggle("hidden");
+    });
+  };
+
+  return (
+    <div>
+      <div id={`subtopic-${props.topicId}`} className="flex justify-between items-center bg-plum-100 p-4 rounded-2xl mb-2 subtopic">
+        <a href={`edit/${props.topicId}`} className="text-leaf-300 pr-2">
+          <i className="fa-solid fa-pen"></i>
+        </a>
+        <a href={`/topicDetails/${props.topicId}`} className="flex-grow">
           <div className="flex items-center space-x-2 cursor-pointer">
-              <a href={`edit/${props.topicId}`} className="text-leaf-300">
-                <i className="fa-solid fa-pen"></i>
-              </a>
-              <h2 className="text-black">{props.topicName}</h2>
-            </div>
-          </a>
-          {props.hasSubTopics && (
-            <button onClick={toggleSubTopics}>
-              <span className="flex justify-center items-center w-6 h-6 bg-leaf-100 rounded-full">
-                <i className="fa-solid fa-caret-down"></i>
-              </span>
-            </button>
-          )}
-        </div>
-        {props.hasSubTopics && props.subTopics && (
-          props.subTopics.map((subTopic, index) => (
-            <div key={index} className={`subtopic-${props.topicId} hidden`}>
-              {subTopic}
-            </div>
-          ))
+            <h2 className="text-black">{props.topicName}</h2>
+          </div>
+        </a>
+        {props.hasSubTopics && (
+          <button onClick={toggleSubTopics}>
+            <span className="flex justify-center items-center w-6 h-6 bg-leaf-100 rounded-full">
+              <i className="fa-solid fa-caret-down"></i>
+            </span>
+          </button>
         )}
       </div>
-    );
-  };
-  
-  export default SubTopicCard;
+      {props.hasSubTopics &&
+        props.subTopics &&
+        props.subTopics.map((subTopic, index) => (
+          <div key={index} className={`subtopic-${props.topicId} hidden`}>
+            {subTopic}
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default SubTopicCard;
--- src/components/TopicCard.tsx
+++ src/components/TopicCard.tsx
@@ -1,11 +1,10 @@
-'use client';
+"use client";
 import { useEffect } from "react";
 export interface Props {
   topicId: number;
   topicName: string;
   parentId: number | null;
   hasSubTopics: boolean;
-
 }
 
 const TopicCard = (props: Props) => {
@@ -15,47 +14,45 @@ const TopicCard = (props: Props) => {
 
   const hideSubTopics = () => {
     let subTopics = document.querySelectorAll(`.subtopic-${props.topicId}`);
-    subTopics.forEach(subTopic => {
-      subTopic.classList.add('hidden');
+    subTopics.forEach((subTopic) => {
+      subTopic.classList.add("hidden");
     });
   };
 
   const toggleSubTopics = () => {
     let subTopics = document.querySelectorAll(`.subtopic-${props.topicId}`);
-    subTopics.forEach(subTopic => {
-      subTopic.classList.toggle('hidden');
+    subTopics.forEach((subTopic) => {
+      subTopic.classList.toggle("hidden");
     });
   };
 
   if (!props.hasSubTopics) {
     return (
-      <a href={`/topicDetails/${props.topicId}`} className="block">
-        <div className="flex justify-between bg-leaf-200 p-5 rounded-2xl mb-2">
-          <div className="flex justify-start cursor-pointer">
-            <a href={`edit/${props.topicId}`} className="pr-2 text-leaf-300">
-              <i className="fa-solid fa-pen"></i>
-            </a>
+      <div className="bg-leaf-200 p-5 rounded-2xl mb-2">
+        <div className="flex justify-start cursor-pointer">
+          <a href={`edit/${props.topicId}`} className="pr-2 text-leaf-300">
+            <i className="fa-solid fa-pen"></i>
+          </a>
+          <a href={`/topicDetails/${props.topicId}`} className="flex-grow flex justify-between">
             <h2 className="text-black">{props.topicName}</h2>
-          </div>
-          <i className="fa-solid fa-caret-right text-black"></i> 
+            <i className="fa-solid fa-caret-right text-black"></i>
+          </a>
         </div>
-      </a>
+      </div>
     );
   } else {
     return (
-      <div className="flex justify-between bg-leaf-200 p-5 rounded-2xl mb-2">
-            <a href={`/topicDetails/${props.topicId}`} className="flex-grow">
-            <div className="flex justify-start cursor-pointer">
-                <a href={`edit/${props.topicId}`} className="pr-2 text-leaf-300">
-                    <i className="fa-solid fa-pen"></i>
-                </a>
-              <h2 className="text-black">{props.topicName}</h2>
-            </div>
-        </a>
-        <button onClick={toggleSubTopics}>
-          <span className="flex justify-center items-center w-6 h-6 bg-leaf-100 rounded-full">
-            <i className="fa-solid fa-caret-down"></i>
-          </span>
+      <div className="flex justify-between items-center bg-leaf-200 parent-padding rounded-2xl mb-2">
+        <div className="flex justify-start cursor-pointer">
+          <a href={`edit/${props.topicId}`} className="pr-2 text-leaf-300">
+            <i className="fa-solid fa-pen"></i>
+          </a>
+          <a href={`/topicDetails/${props.topicId}`} className="flex-grow">
+            <h2 className="text-black">{props.topicName}</h2>
+          </a>
+        </div>
+        <button onClick={toggleSubTopics} className="flex justify-center items-center w-6 h-6 bg-leaf-100 rounded-full">
+          <i className="fa-solid fa-caret-down"></i>
         </button>
       </div>
     );
