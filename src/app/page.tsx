f330acea93b4c0bb1c1591107e09f236880fa5e5
emmaohn <joh20123@byui.edu>
2024-07-20 21:50:45 -0600

    temp pt2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,63 +1,57 @@
 'use client'
-import { Metadata } from "next";
 import { useState, useEffect } from "react";
 
 import TopicCard from "../components/TopicCard";
 import SubTopicCard from "@/components/SubTopicCard";
 
 // import { getAllTopics, getAllTags, getAllDeleted } from "@/database/database";
-import { Topic, Tag } from "@prisma/client";
-
-export const metadata: Metadata = {
-  title: 'Topics',
-};
+import { Topic, Tag, Attachment, Link, Note, Photo } from "@prisma/client";
 
 export default async function Home() {
-  const [topics, setTopics] = useState<{ topic_id: number, topic_name: string }[]>([]);
-  const [tags, setTags] = useState<{ tag_id: number, tag: string }[]>([]);
-  const [deletedTopics, setDeletedTopics] = useState<{ topic_id: number, topic_name: string }[]>([]);
-
+  const [topics, setTopics] = useState<Topic[]>([]);
+  const [tags, setTags] = useState<Tag[]>([]);
+  const [deletedItems, setdeletedItems] = useState<[Attachment[], Link[], Note[], Photo[]]>();
+  const [tagSelectValue, setTagSelectValue] = useState<string | null>("");
 
   useEffect(() => {
-    const fetchTopicsAndTags = async () => {
+    const fetchTopicsTagsAndDeleted = async () => {
       try {
-        const [topicsResponse, tagsResponse, deletedTopicsResponse] = await Promise.all([
+        const [topicsResponse, tagsResponse, deletedItemsResponse] = await Promise.all([
           fetch('/api/topics/get'),
           fetch('/api/tags/get'),
-          fetch('/api/topics/deleted/get'),
+          fetch('/api/deleted/get'),
         ]);
 
         const topicsData = await topicsResponse.json();
         const tagsData = await tagsResponse.json();
-        const deletedTopicsData = await deletedTopicsResponse.json();
+        const deletedItemsData = await deletedItemsResponse.json();
 
         setTopics(topicsData);
         setTags(tagsData);
-        setDeletedTopics(deletedTopicsData);
+        setdeletedItems(deletedItemsData);
       } catch (error) {
         console.error('Error fetching topics or tags:', error);
       }
     };
-  let topicList: Topic[] = await getAllTopics();
-  let tagList: Tag[] = await getAllTags();
-  let deletedTopicsList: any = await getAllDeleted();
-  let deletedCount: number = deletedTopicsList[0].length + deletedTopicsList[1].length + deletedTopicsList[2].length + deletedTopicsList[3].length
+    fetchTopicsTagsAndDeleted();
+  }, []);
+
+  let deletedCount: number = 0;
+  deletedItems ? deletedCount = deletedItems[0].length + deletedItems[1].length + deletedItems[2].length + deletedItems[3].length : 0;
 
   const topicsWithSubtopics = new Set<number>();
-  topicList.forEach(topic => {
+  topics.forEach(topic => {
     if (topic.parent_id !== null) {
       topicsWithSubtopics.add(topic.parent_id);
     }
   });
 
-  const [tagSelectValue, setTagSelectValue] = useState<string | null>("");
   const handleTagChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const value = e.target.value;
     setTagSelectValue(value === "" ? null : value);
-  };
+  }
   const filterTopics = (tagId: number) => {
-    topicList = topicList.filter((topic: any) => {return topic.Topic_Tag.includes(tagId)});
-    return topicList;
+    setTopics(topics.filter((topic: any) => {return topic.Topic_Tag.includes(tagId)}))
   }
 
   const renderTopics = (topics: Topic[], parentId: number | null = null, level: number = 0) => {
@@ -95,7 +89,7 @@ export default async function Home() {
       <select onChange={handleTagChange} className="border-4 border-leaf-200 bg-inherit rounded-xl my-3 p-1.5 w-full text-leaf-300 font-bold">
         <option className="text-sm" value="">Filter by</option>
         {
-          tagList.map(tag => {
+          tags.map(tag => {
             return (<option className="text-sm" value={tag.tag_id}>{tag.tag}</option>);
           })
         }
@@ -103,7 +97,7 @@ export default async function Home() {
       <a href="/deleted" className="flex justify-center items-center gap-1 bg-plum-100 rounded-xl my-3 p-2 w-full text-plum-300 font-bold">Deleted<span className="bg-plum-300 text-plum-100 text-xs px-1.5 py-0.5 rounded-full">{deletedCount}</span></a>
     </div>
       <h1>Topics</h1>
-      {renderTopics(topicList)}
+      {renderTopics(topics)}
     </main>
   );
 }
